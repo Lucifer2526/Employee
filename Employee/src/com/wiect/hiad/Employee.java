c769c2a0535900043ffa1c321856f5788a160022
Adi <Adi@192.168.48.75>
2024-06-29 12:24:07 +0530

    Employee encaps

---
+++ Employee/src/com/wiect/hiad/Employee.java
@@ -0,0 +1,47 @@
+package com.wiect.hiad;
+
+public class Employee {
+	private String name;
+	 private int id;
+	 private float salary;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the salary
+	 */
+	public float getSalary() {
+		return salary;
+	}
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", id=" + id + ", salary=" + salary + "]";
+	}
+}
---
+++ Employee/src/com/wiect/hiad/EmployeeDemo.java
@@ -0,0 +1,19 @@
+package com.wiect.hiad;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee s1=new Employee();
+		s1.setName("ADITYA SINGH");
+//		System.out.println("name of s1 is " +s1.getName());
+		
+		s1.setId(46);
+//		System.out.println("name of s1 is " +s1.getId());
+		
+		s1.setSalary(45000);
+//		System.out.println("name of s1 is " +s1.getSalary());
+		System.out.println(s1);
+	}
+
+}
